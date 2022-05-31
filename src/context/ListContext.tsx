c6882eb2f741da44a4b1c422880f1b741208a7d6
Nimesha Gunawardana <nimeshae@gmail.com>
2022-06-01 01:04:47 +0530

    List and Task context created

---
+++ src/context/ListContext.tsx
@@ -0,0 +1,52 @@
+import React, { createContext, useReducer, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { Action, ActionType, List } from '../types/List';
+
+type ListState = typeof initialState;
+
+interface ListContextInterface {
+  list: List[];
+  dispatch: React.Dispatch<Action>;
+}
+
+const initialState = JSON.parse(localStorage.getItem('List') as string) || {
+  id: '',
+  title: '',
+  value: '',
+};
+
+export const ListContext = createContext<ListContextInterface | null>(null);
+
+export const listReducer = (list: ListState[], action: Action) => {
+  switch (action.type) {
+    case ActionType.ADD_LIST:
+      return [
+        ...list,
+        {
+          id: uuidv4(),
+          title: action.payload.title,
+          value: action.payload.value,
+        },
+      ];
+    default:
+      return list;
+  }
+};
+
+export const ListContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [list, dispatch] = useReducer(listReducer, initialState);
+
+  useEffect(() => {
+    localStorage.setItem('List', JSON.stringify(list));
+  }, [list]);
+
+  return (
+    <ListContext.Provider value={{ list, dispatch }}>
+      {children}
+    </ListContext.Provider>
+  );
+};
---
+++ src/context/TaskContext.tsx
@@ -0,0 +1,71 @@
+import React, { createContext, useReducer, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { Action, ActionType, Task } from '../types/Task';
+import moment from 'moment';
+
+type TaskState = typeof initialState;
+
+interface TaskContextInterface {
+  taskCard: Task[] | any;
+  dispatch: React.Dispatch<Action>;
+}
+
+const initialState = JSON.parse(localStorage.getItem('Task') as string) || {
+  id: '',
+  title: '',
+  description: '',
+  state: '',
+};
+
+export const TaskContext = createContext<TaskContextInterface | null>(null);
+
+export const listReducer = (taskCard: TaskState[], action: Action) => {
+  switch (action.type) {
+    case ActionType.ADD_TASK:
+      return [
+        ...taskCard,
+        {
+          id: uuidv4(),
+          title: action.payload.title,
+          description: action.payload.description,
+          state: action.payload.state,
+          dateTime: moment().format(),
+        },
+      ];
+    case ActionType.UPDATE_TASK:
+      const index: number = taskCard.findIndex(
+        (item) => item?.id === action.payload.id
+      );
+      taskCard[index]!.title = action.payload.title;
+      taskCard[index]!.description = action.payload.description;
+      taskCard[index]!.state = action.payload.state;
+      taskCard[index]!.dateTime = moment().format();
+
+      return [...taskCard];
+    case ActionType.REMOVE_TASK:
+      return taskCard.filter((item) => item?.id !== action.payload.id);
+    default:
+      return taskCard;
+  }
+};
+
+export const TaskContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [taskCard, dispatch] = useReducer(listReducer, initialState);
+
+  useEffect(() => {
+    localStorage.setItem('Task', JSON.stringify(taskCard));
+  }, [taskCard]);
+
+  return (
+    <TaskContext.Provider value={{ taskCard, dispatch }}>
+      {children}
+    </TaskContext.Provider>
+  );
+};
+// function a(a: any) {
+//   throw new Error('Function not implemented.');
+// }
